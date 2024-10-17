2049425c88f33c68568edfa7e883874beb14b7e3
celikerde <celikerde@itu.edu.tr>
2024-10-17 15:15:56 +0300

    extension are completed

--- provider_extension.dart
+++ provider_extension.dart
@@ -1,1 +1,46 @@
-void main() {}
+void main() {
+  final context = BuildContext();
+  final products = context.read<ProductViewModel>().products;
+  print(products?[0].name);
+}
+
+class BuildContext {}
+
+class Provider {
+  static T of<T>(BuildContext context, {bool listen = true}) {
+    return ProductViewModel() as T;
+  }
+}
+
+extension _ProviderMixin on BuildContext {
+  T watch<T>() => Provider.of<T>(this);
+  T read<T>() => Provider.of<T>(
+        this,
+        listen: false,
+      );
+}
+
+class ProductViewModel {
+  List<Product>? _products;
+  List<Product>? get products => _products;
+
+  ProductViewModel() {
+    setProducts();
+  }
+
+  void setProducts() {
+    _products = [
+      Product(name: 'Monster', price: 345345),
+      Product(name: 'HAcer', price: 21131),
+    ];
+  }
+}
+
+class Product {
+  final String? name;
+  final double? price;
+  const Product({
+    this.name,
+    this.price,
+  });
+}
